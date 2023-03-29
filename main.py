1d761a2441111021a16d9f561088a9ddf1a13987
LFerrin24 <825875@mystma.org>
2023-03-29 15:04:51 -0500

    2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
 def hasL(w):
+    numL = 0
     for i in range(0,len(w)):
         if w[i] == "l":
+            numL = numL + 1
             return True
         else:
             return False
+
 
 
 
